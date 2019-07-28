66032b0a2ddb092767215b9a1c4e46a0931b94af
Ranjan Sai <mail2javawiz@gmail.com>
2019-07-28 14:06:31 +0530

    updated

--- src/main/java/com/javawiz/entity/Customer.java
+++ src/main/java/com/javawiz/entity/Customer.java
@@ -20,8 +20,8 @@ import lombok.ToString;
 public class Customer {
  
 	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "prooduct_id_seq")
-	@SequenceGenerator(name = "prooduct_id_seq", sequenceName = "PRODUCT_ID_SEQ", allocationSize = 100)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "cust_id_seq")
+	@SequenceGenerator(name = "cust_id_seq", sequenceName = "CUSTOMER_ID_SEQ", allocationSize = 100)
     private Long id;
     
     @Column(name = "FIRSTNAME")
